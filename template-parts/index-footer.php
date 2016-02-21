cd47e222a0865d460fdb97c38f12678b6ca20aa4
deeem <karpov.dimon@gmail.com>
2016-02-21 18:15:53 +0200

    Clean Page Template

--- template-parts/index-footer.php
+++ template-parts/index-footer.php
@@ -1,4 +1,6 @@
-<section id="page-footer" class="feature-image-default-alt" data-type="background" data-speed="2">
+<section id="page-footer"
+         class="feature-image-default-alt"
+         data-type="background" data-speed="2">
 	<div class="container">
 		<h1>&nbsp;</h1>
 	</div>
